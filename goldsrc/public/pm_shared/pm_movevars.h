8e94eb3682280ca108267c98dda362639b847298
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2021-12-02 12:32:42 +0000

    [PM_Shared] Update/complete the movevars_t struct

--- goldsrc/public/pm_shared/pm_movevars.h
+++ goldsrc/public/pm_shared/pm_movevars.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of OGS Engine
  * Copyright (C) 1996-1997 Id Software, Inc.
- * Copyright (C) 2018 BlackPhrase
+ * Copyright (C) 2018, 2020-2021 BlackPhrase
  *
  * OGS Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,23 +24,39 @@
 
 typedef struct movevars_s
 {
-	float	gravity;
-	float	stopspeed;
-	float	maxspeed;
-	float	spectatormaxspeed;
-	float	accelerate;
-	float	airaccelerate;
-	float	wateraccelerate;
-	float	friction;
-	float	edgefriction;
-	float	waterfriction;
-	float	entgravity;
-	float	bounce;
-	float	stepsize;
-	float	maxvelocity;
+	float gravity;
+	float stopspeed;
+	float maxspeed;
+	float spectatormaxspeed;
+	float accelerate;
+	float airaccelerate;
+	float wateraccelerate;
+	float friction;
+	float edgefriction;
+	float waterfriction;
+	float entgravity;
+	float bounce;
+	float stepsize;
+	float maxvelocity;
 	
+	float zmax;
+	float waveHeight;
+	
+	qboolean footsteps;
+	
+	char skyName[32];
+	
+	// These two are probably used only by the client-side...?
 	float rollangle; // = cvar_t cl_rollangle
 	float rollspeed; // = cvar_t cl_rollspeed
+	
+	float skycolor_r;
+	float skycolor_g;
+	float skycolor_b;
+	
+	float skyvec_x;
+	float skyvec_y;
+	float skyvec_z;
 } movevars_t;
 
 extern movevars_t movevars;
