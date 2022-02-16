781d2214b9cc71c7a191f01c64ced100438c8fd3
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2022-02-16 14:28:19 +0000

    [Common] Rewrite the dll_state header

--- goldsrc/common/dll_state.h
+++ goldsrc/common/dll_state.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of OGS Engine
- * Copyright (C) 2018 BlackPhrase
+ * Copyright (C) 2018, 2022 BlackPhrase
  *
  * OGS Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,3 +19,24 @@
 /// @file
 
 #pragma once
+
+/// DLL State Flags
+enum
+{
+	DLL_INACTIVE = 0, ///< No DLL
+	DLL_ACTIVE, ///< DLL is running
+	DLL_PAUSED, ///< DLL is paused
+	DLL_CLOSE, ///< Closing down DLL
+	DLL_TRANS ///< Level transition
+};
+
+/// DLL Pause reasons
+enum
+{
+	DLL_NORMAL = 0, ///< User hit ESC or something
+	DLL_QUIT = 4, ///< Quit now
+	DLL_RESTART = 6 ///< Switch to launcher for Linux, does a quit but returns 1
+};
+
+/// DLL Substate info (not relevant)
+#define ENG_NORMAL (1 << 0)
