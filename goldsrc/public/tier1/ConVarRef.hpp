c7c8eadea93a2fdf9ecfd869cde1e602c8218859
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2023-06-17 16:20:39 +0000

    [Public/Tier1] CConVarRef.hpp -> ConVarRef.hpp

--- goldsrc/public/tier1/ConVarRef.hpp
+++ goldsrc/public/tier1/ConVarRef.hpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of OGS Engine SDK
  *
- * Copyright (C) 2015-2018, 2023 BlackPhrase
+ * Copyright (C) 2023 BlackPhrase
  *
  * OGS Engine SDK is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
